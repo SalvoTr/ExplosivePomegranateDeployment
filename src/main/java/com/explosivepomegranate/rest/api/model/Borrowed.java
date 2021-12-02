7f3499f1eaaa971c7df073c070e1ae1c9c4fcf2f
sonja.nussbaumer <sonja.nussbaumer@students.fhnw.ch>
2021-12-02 17:48:40 +0100

    definition book status meaning

--- src/main/java/com/explosivepomegranate/rest/api/model/Borrowed.java
+++ src/main/java/com/explosivepomegranate/rest/api/model/Borrowed.java
@@ -17,7 +17,7 @@ public class Borrowed {
     private Date init_end_date;
     private Date extend_end_date;
     private String book_comment;
-    private boolean book_status ;
+    private boolean book_status; // true means book is borrowed, false means available
 
     //Connects to book
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
