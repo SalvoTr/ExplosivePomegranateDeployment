322e1f7eadee80484312afefd3661d8eb5a0ce33
Clelia Meneghin <cmeneghi@adobe.com>
2021-12-02 21:53:17 +0100

    load books from db and logout

---
+++ src/main/resources/static/assets/js/app.js
@@ -0,0 +1,16 @@
+serviceEndpointURL = window.location.protocol + "//" + window.location.host;
+
+// make a Get reguest to "/allBooks" and get the json with all books back
+function getAllBooks(callback) {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: serviceEndpointURL + "/allBooks",
+        success: function (data) {
+            callback(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR, textStatus, errorThrown);
+        }
+    });
+}
