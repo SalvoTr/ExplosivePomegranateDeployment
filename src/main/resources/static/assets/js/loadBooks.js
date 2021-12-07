630a4d23081ea1d55f4be818e3f517b3c3be97ab
Clelia Meneghin <cmeneghi@adobe.com>
2021-12-07 20:27:22 +0100

    make card clickable

--- src/main/resources/static/assets/js/loadBooks.js
+++ src/main/resources/static/assets/js/loadBooks.js
@@ -12,7 +12,8 @@ $(document).ready(function () {
                 //add new item into div with id allBooks
                 $("#allBooks")
                     .append(
-                        $('<a href="/bookDetails/'+book.book_id+'"><div class="row">').append(
+                        $('<div class="row rowList">').append(
+                            $('<a href="/bookDetails/'+book.book_id+'">'),
                             $('<div class="col-sm-2">').append(
                                 $('<img src="../assets/img/sample-book-cover.png">')
                             ),
@@ -20,9 +21,9 @@ $(document).ready(function () {
                                 $(book.currentlyBorrowed ? $('<p class="BorrowedStyle">').text("Currently borrowed") : $('<p class="notBorrowedStyle">').text("Available")),
                                 $("<h3>").text(book.title),
                                 $("<p>").text(book.description)
-                            ),
-                            $('<div class="placeholder-empty">')
-                        )
+                            )
+                        ),
+                        $('<div class="placeholder-empty">')
                     );
 
             })
